a512c5892bff87be2a2531b41c26e30978eb57b7
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-03-26 23:06:28 +0300

    feat: add Header component

---
+++ components/src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import style from './Header.scss';
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.headerContainer}>
+        <nav className={style.headerMenu}>
+          <ul className={style.menuList}>
+            <li>
+              <NavLink to="/" className={style.menuLink}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={style.menuLink}>
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- components/src/pages/HomePage/HomePage.tsx
+++ components/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../../components/Layout/Layout';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default HomePage;
