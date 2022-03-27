dd723309a8082b1f0b80affd04814a6cf39f2047
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-03-28 02:33:10 +0300

    feat: add Cards component

---
+++ components/src/components/Cards/Cards.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+import style from './Cards.module.scss';
+
+type CardProps = {
+  id: string;
+  title: string;
+  author: string;
+  url: string;
+  date: string;
+  views: string;
+  likes: string;
+  dislikes: string;
+  comments: string;
+};
+
+type CardsProps = {
+  cards: CardProps[];
+};
+
+type CardsState = {
+  state: Record<string, never>;
+};
+
+class Cards extends React.Component<CardsProps, CardsState> {
+  render() {
+    return (
+      <div className={style.cards}>
+        {this.props.cards.map((card) => (
+          <Card key={this.props.cards.indexOf(card)} {...card} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Cards;
--- components/src/pages/HomePage/HomePage.tsx
+++ components/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,104 @@
 import React from 'react';
-import Header from '../../components/Layout/Layout';
+import Cards from '../../components/Cards/Cards';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const propsValues = [
+  {
+    id: '1',
+    title: 'Portrait of a cat looking at the camera',
+    author: 'Lloyd Henneman',
+    url: '',
+    date: 'March 26th, 2022',
+    views: '1000',
+    likes: '500',
+    dislikes: '200',
+    comments: '100',
+  },
+  {
+    id: '1',
+    title: 'Portrait of a cat looking at the camera',
+    author: 'Lloyd Henneman',
+    url: '',
+    date: 'March 26th, 2022',
+    views: '1000',
+    likes: '500',
+    dislikes: '200',
+    comments: '100',
+  },
+  {
+    id: '1',
+    title: 'Portrait of a cat looking at the camera',
+    author: 'Lloyd Henneman',
+    url: '',
+    date: 'March 26th, 2022',
+    views: '1000',
+    likes: '500',
+    dislikes: '200',
+    comments: '100',
+  },
+  {
+    id: '1',
+    title: 'Portrait of a cat looking at the camera',
+    author: 'Lloyd Henneman',
+    url: '',
+    date: 'March 28th, 2022',
+    views: '1000',
+    likes: '500',
+    dislikes: '200',
+    comments: '100',
+  },
+  {
+    id: '1',
+    title: 'Portrait of a cat looking at the camera',
+    author: 'Lloyd Henneman',
+    url: '',
+    date: 'March 28th, 2022',
+    views: '1000',
+    likes: '500',
+    dislikes: '200',
+    comments: '100',
+  },
+];
+
+type HomePageProps = Record<string, never>; //{
+// props: Record<string, never>;
+//};
+type HomePageState = {
+  searchValue: string;
 };
+
+class HomePage extends React.Component<HomePageProps, HomePageState> {
+  constructor(props: HomePageProps) {
+    super(props);
+    this.state = {
+      searchValue: '',
+    };
+    this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
+  }
+
+  handleSearchBarChange(value: string) {
+    this.setState({ searchValue: value });
+  }
+
+  componentDidMount() {
+    this.setState({ searchValue: localStorage.getItem('searchValue') || '' });
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('searchValue', this.state.searchValue);
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchValue={this.state.searchValue}
+          onSearchBarChange={this.handleSearchBarChange}
+        />
+        <Cards cards={propsValues} />
+      </div>
+    );
+  }
+}
 
 export default HomePage;
