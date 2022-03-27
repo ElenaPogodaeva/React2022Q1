3a35fec14074b16e18e3d50a852005f295c9703d
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-03-28 02:13:26 +0300

    feat: add Card component

---
+++ components/src/components/Card/Card.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import style from './Card.module.scss';
+
+type CardProps = {
+  id: string;
+  title: string;
+  author: string;
+  url: string;
+  date: string;
+  views: string;
+  likes: string;
+  dislikes: string;
+  comments: string;
+};
+
+type CardState = Record<string, never>;
+
+class Card extends React.Component<CardProps, CardState> {
+  render() {
+    return (
+      <div className={style.card}>
+        <img src={require(`../../assets/img/${this.props.id}.jpg`)} className={style.cardImg}></img>
+        <div className={style.cardBody}>
+          <p className={style.cardTitle}>{this.props.title}</p>
+          <p className={style.cardOwner}>By {this.props.author}</p>
+          <div className={style.cardDateViewWrapper}>
+            <p className={style.cardDate}>{this.props.date}</p>
+            <p className={style.cardViews}>Views: {this.props.views}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
