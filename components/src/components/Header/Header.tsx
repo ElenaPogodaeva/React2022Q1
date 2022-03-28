5de428409421456a66a49692d8e9c0613fb7ccb2
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-03-29 00:16:07 +0300

    fix: fix styles

--- components/src/components/Header/Header.tsx
+++ components/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import style from './Header.scss';
+import style from './Header.module.scss';
 
 const Header = () => {
   return (
