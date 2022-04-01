a8fd20366726b4b397e5bf02b20dec96d9ed4540
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-01 14:00:17 +0300

    refactor: refactor Cards

--- components/src/components/Cards/Cards.tsx
+++ components/src/components/Cards/Cards.tsx
@@ -29,8 +29,7 @@ class Cards extends React.Component<CardsProps, CardsState> {
 
     return (
       <div className={style.cards} data-testid="cards">
-        {this.props.cards &&
-          this.props.cards.map((card) => <Card key={card.id} {...card} />)}
+        {this.props.cards && this.props.cards.map((card) => <Card key={card.id} {...card} />)}
       </div>
     );
   }
