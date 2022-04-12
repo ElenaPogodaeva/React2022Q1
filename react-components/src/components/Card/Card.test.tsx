a3bf2c3a6d0432349305c42991c0370d0cf8bd3d
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-12 14:14:10 +0300

    fix: fix Card

--- react-components/src/components/Card/Card.test.tsx
+++ react-components/src/components/Card/Card.test.tsx
@@ -1,21 +1,29 @@
 import { render } from '@testing-library/react';
 import Card from './Card';
 
-const propValue = {
-  id: '1',
-  title: 'Portrait of a cat looking at the camera',
-  author: 'Lloyd Henneman',
-  url: '',
-  date: 'March 26th, 2022',
-  views: '1000',
-  likes: '500',
-  dislikes: '200',
-  comments: '100',
+const mockResponse = {
+  id: '51997149627',
+  owner: '15339304@N06',
+  secret: '019125af5f',
+  server: '65535',
+  farm: 66,
+  title: '7K8A8825',
+  ispublic: 1,
+  isfriend: 0,
+  isfamily: 0,
+  datetaken: '2022-03-25 08:58:40',
+  datetakengranularity: 0,
+  datetakenunknown: 0,
+  ownername: 'rpealit',
+  views: 0,
+  url_n: 'https://live.staticflickr.com/65535/51997149627_019125af5f_n.jpg',
+  height_n: '202',
+  width_n: '320',
 };
 
 describe('Card component', () => {
   it('Card renders', () => {
-    const { getByTestId } = render(<Card {...propValue} />);
+    const { getByTestId } = render(<Card {...mockResponse} />);
     const card = getByTestId('card');
     expect(card).toBeDefined();
   });
--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -1,42 +1,23 @@
 import React from 'react';
-
+import { Image } from '../../types/types';
 import style from './Card.module.scss';
 
-type CardProps = {
-  id: string;
-  title: string;
-  author: string;
-  url: string;
-  date: string;
-  views: string;
-  likes: string;
-  dislikes: string;
-  comments: string;
-};
-
+type CardProps = Image;
 type CardState = Record<string, never>;
 
 class Card extends React.Component<CardProps, CardState> {
   render() {
     return (
       <div className={style.card} data-testid="card">
-        <img src={require(`../../../public/img/${this.props.id}.jpg`)} className={style.cardImg} />
+        <img src={this.props.url_n} className={style.cardImg} />
         <div className={style.cardBody}>
           <p className={style.cardTitle}>{this.props.title}</p>
-          <p className={style.cardOwner}>By {this.props.author}</p>
-          <p className={style.cardDate}>{this.props.date}</p>
-          <div className={style.cardStatistics}>
-            <div className={style.statisticsItem}>
-              <div className={`${style.statisticsImg} ${style.statisticsViews}`}></div>
+          <p className={style.cardOwner}>By {this.props.ownername}</p>
+          <div className={style.cardDateViewWrapper}>
+            <p className={style.cardDate}>{this.props.datetaken}</p>
+            <div className={style.cardViews}>
+              <div className={style.cardViewsImg}></div>
               {this.props.views}
-            </div>
-            <div className={style.statisticsItem}>
-              <div className={`${style.statisticsImg} ${style.statisticsLikes}`}></div>
-              {this.props.likes}
-            </div>
-            <div className={style.statisticsItem}>
-              <div className={`${style.statisticsImg} ${style.statisticsComments}`}></div>
-              {this.props.comments}
             </div>
           </div>
         </div>
