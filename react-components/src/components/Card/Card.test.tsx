cdb772917ed3b044222ae9e95214c91ec2703822
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-13 21:39:05 +0300

    fix: fix Card

--- react-components/src/components/Card/Card.test.tsx
+++ react-components/src/components/Card/Card.test.tsx
@@ -4,16 +4,8 @@ import Card from './Card';
 const mockResponse = {
   id: '51997149627',
   owner: '15339304@N06',
-  secret: '019125af5f',
-  server: '65535',
-  farm: 66,
   title: '7K8A8825',
-  ispublic: 1,
-  isfriend: 0,
-  isfamily: 0,
   datetaken: '2022-03-25 08:58:40',
-  datetakengranularity: 0,
-  datetakenunknown: 0,
   ownername: 'rpealit',
   views: 0,
   url_n: 'https://live.staticflickr.com/65535/51997149627_019125af5f_n.jpg',
@@ -21,9 +13,11 @@ const mockResponse = {
   width_n: '320',
 };
 
+const onClick = jest.fn();
+
 describe('Card component', () => {
   it('Card renders', () => {
-    const { getByTestId } = render(<Card {...mockResponse} />);
+    const { getByTestId } = render(<Card card={mockResponse} handleClick={onClick} />);
     const card = getByTestId('card');
     expect(card).toBeDefined();
   });
--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { Image } from '../../types/types';
 import style from './Card.module.scss';
 
-type CardProps = Image;
+type CardProps = {
+  card: Image;
+  handleClick: (id: string) => void;
+};
+
 type CardState = Record<string, never>;
 
 class Card extends React.Component<CardProps, CardState> {
   render() {
+    const { id, url_n, title, ownername, datetaken, views } = this.props.card;
     return (
-      <div className={style.card} data-testid="card">
-        <img src={this.props.url_n} className={style.cardImg} />
+      <div className={style.card} data-testid="card" onClick={() => this.props.handleClick(id)}>
+        <img src={url_n} className={style.cardImg} />
         <div className={style.cardBody}>
-          <p className={style.cardTitle}>{this.props.title}</p>
-          <p className={style.cardOwner}>By {this.props.ownername}</p>
+          <p className={style.cardTitle}>{title}</p>
+          <p className={style.cardOwner}>By {ownername}</p>
           <div className={style.cardDateViewWrapper}>
-            <p className={style.cardDate}>{this.props.datetaken}</p>
+            <p className={style.cardDate}>{datetaken}</p>
             <div className={style.cardViews}>
               <div className={style.cardViewsImg}></div>
-              {this.props.views}
+              {views}
             </div>
           </div>
         </div>
