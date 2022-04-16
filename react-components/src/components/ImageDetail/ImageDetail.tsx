226129db2948936ea91825e76e86e4802d7a7096
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-16 14:35:26 +0300

    fix: fix ImageDetail

--- react-components/src/components/ImageDetail/ImageDetail.tsx
+++ react-components/src/components/ImageDetail/ImageDetail.tsx
@@ -11,31 +11,28 @@ type CardProps = {
 type CardState = Record<string, never>;
 
 class ImageDetail extends React.Component<CardProps, CardState> {
+  stopPropagation(e: React.MouseEvent<HTMLElement>) {
+    e.stopPropagation();
+  }
+
   render() {
     const { title, owner, description, dates, views } = this.props.imageInfo;
-    //const comments = this.props.imageComments;
     const url = this.props.imageUrl;
 
     return (
       <div className={style.popup} data-testid="image-detail">
-        <div className={style.popupBody}>
-          <div className={style.popupContent}>
+        <div className={style.popupBody} onClick={this.props.handleClose}>
+          <div className={style.popupContent} onClick={this.stopPropagation}>
             <img src={url} className={style.popupImg} />
             <h1 className={style.popupTitle}>{title._content}</h1>
-            <p className={style.popupOwner}>By {owner.username}</p>
-            <p className={style.popupDate}>Taken on {dates.taken}</p>
-            <p className={style.popupDate}>Views: {views}</p>
+            <p className={style.popupText}>By {owner.username}</p>
+            <p className={style.popupText}>Taken on {dates.taken}</p>
+            <p className={style.popupText}>Views: {views}</p>
             <h2 className={style.popupSubtitle}>Description</h2>
-            <p className={style.popupText}>{description._content}</p>
-            {/* <ul className={style.tagList}>
-              {comments &&
-                comments.map((comment) => (
-                  <li key={comment.id}>
-                    <p>{comment.authorname}</p>
-                    <p>{comment._content}</p>
-                  </li>
-                ))}
-            </ul> */}
+            <p
+              className={style.popupDesc}
+              dangerouslySetInnerHTML={{ __html: description._content }}
+            ></p>
             <button className={style.closeBtn} onClick={this.props.handleClose}></button>
           </div>
         </div>
