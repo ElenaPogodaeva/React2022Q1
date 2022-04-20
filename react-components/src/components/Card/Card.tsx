db83171345d0a327359fe41af2004c6e04eda351
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-21 00:31:00 +0300

    feat: convert Card to a function component

--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -7,28 +7,25 @@ type CardProps = {
   handleClick: (id: string) => void;
 };
 
-type CardState = Record<string, never>;
+export const Card = ({ card, handleClick }: CardProps) => {
+  const { id, url_n, title, ownername, datetaken, views } = card;
 
-class Card extends React.Component<CardProps, CardState> {
-  render() {
-    const { id, url_n, title, ownername, datetaken, views } = this.props.card;
-    return (
-      <div className={style.card} data-testid="card" onClick={() => this.props.handleClick(id)}>
-        <img src={url_n} className={style.cardImg} />
-        <div className={style.cardBody}>
-          <p className={style.cardTitle}>{title}</p>
-          <p className={style.cardOwner}>By {ownername}</p>
-          <div className={style.cardDateViewWrapper}>
-            <p className={style.cardDate}>{datetaken}</p>
-            <div className={style.cardViews}>
-              <div className={style.cardViewsImg}></div>
-              {views}
-            </div>
+  return (
+    <div className={style.card} data-testid="card" onClick={() => handleClick(id)}>
+      <img src={url_n} className={style.cardImg} />
+      <div className={style.cardBody}>
+        <p className={style.cardTitle}>{title}</p>
+        <p className={style.cardOwner}>By {ownername}</p>
+        <div className={style.cardDateViewWrapper}>
+          <p className={style.cardDate}>{datetaken}</p>
+          <div className={style.cardViews}>
+            <div className={style.cardViewsImg}></div>
+            {views}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
