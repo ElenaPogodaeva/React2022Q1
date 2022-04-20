2f28dada8ff0ef82f8c7a74cf89d8e3ff61fe9ae
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-20 21:39:13 +0300

    feat: convert HomePage to a function component

--- react-components/src/pages/HomePage/HomePage.tsx
+++ react-components/src/pages/HomePage/HomePage.tsx
@@ -1,36 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Cards from '../../components/Cards/Cards';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Spinner from '../../components/Spinner/Spinner';
 import { Image, SearchImagesParams } from '../../types/types';
 import { flickr } from '../../common/flickr';
 
-type HomePageProps = Record<string, never>;
-type HomePageState = {
-  searchValue: string;
-  images: Image[];
-  isLoading: boolean;
-  error: Error | null;
-};
+export const HomePage = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [images, setImages] = useState<Image[]>([]);
+  const [error, setError] = useState<Error | null>(null);
 
-class HomePage extends React.Component<HomePageProps, HomePageState> {
-  constructor(props: HomePageProps) {
-    super(props);
-    this.state = {
-      searchValue: '',
-      images: [],
-      isLoading: true,
-      error: null,
-    };
-    this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
-    this.handleSearchBarSubmit = this.handleSearchBarSubmit.bind(this);
-  }
+  const searchValueRef = useRef('');
+  searchValueRef.current = searchValue;
 
-  async fetchImages(searchValue: string) {
-    this.setState({
-      isLoading: true,
-    });
-
+  const fetchImages = async (searchValue: string) => {
+    setIsLoading(true);
     const params: SearchImagesParams = {
       tags: searchValue,
       extras: 'url_n,owner_name,date_taken,views',
@@ -41,68 +26,152 @@ class HomePage extends React.Component<HomePageProps, HomePageState> {
 
     try {
       const fetchedImages = await flickr('photos.search', params);
-
-      this.setState({
-        images: fetchedImages.photos.photo.filter((item: Image) => item.url_n),
-      });
-      this.setState({
-        error: null,
-      });
+      setImages(fetchedImages.photos.photo.filter((item: Image) => item.url_n));
+      setError(null);
     } catch (err) {
-      this.setState({
-        images: [],
-      });
-      this.setState({
-        error: err as Error,
-      });
+      setImages([]);
+      setError(error);
     } finally {
-      this.setState({
-        isLoading: false,
-      });
+      setIsLoading(false);
     }
-  }
+  };
 
-  handleSearchBarChange(value: string) {
-    this.setState({ searchValue: value });
-  }
-
-  handleSearchBarSubmit() {
-    this.fetchImages(this.state.searchValue);
-  }
-
-  async componentDidMount() {
+  useEffect(() => {
     const localStorageValue = localStorage.getItem('searchValue');
     if (localStorageValue) {
-      await this.setState({ searchValue: localStorageValue });
-      await this.fetchImages(this.state.searchValue);
+      setSearchValue(localStorageValue);
+      fetchImages(localStorageValue);
     } else {
-      await this.fetchImages('nature,flowers');
+      fetchImages('nature,flowers');
     }
-  }
 
-  componentWillUnmount() {
-    localStorage.setItem('searchValue', this.state.searchValue);
-  }
+    // return () => {
+    //   console.log('unmount');
+    //   localStorage.setItem('searchValue', searchValueRef.current as string);
+    // };
+  }, []);
 
-  render() {
-    const images = this.state.images;
-    const isLoading = this.state.isLoading;
-    const error = this.state.error;
-    const notFound = !error && !isLoading && images.length === 0 ? 'Nothing found' : null;
+  useEffect(() => {
+    return () => {
+      console.log('unmount');
+      localStorage.setItem('searchValue', searchValue);
+    };
+  }, [searchValue]);
 
-    return (
-      <div data-testid="home-page">
-        <SearchBar
-          searchValue={this.state.searchValue}
-          onSearchBarChange={this.handleSearchBarChange}
-          onSearchBarSubmit={this.handleSearchBarSubmit}
-        />
-        {error && <div>Error occured</div>}
-        {isLoading ? <Spinner /> : <Cards cards={this.state.images} />}
-        {notFound}
-      </div>
-    );
-  }
-}
+  const handleSearchBarChange = (value: string) => {
+    setSearchValue(value);
+  };
+
+  const handleSearchBarSubmit = () => {
+    fetchImages(searchValue);
+  };
+
+  const notFound = !error && !isLoading && images.length === 0 ? 'Nothing found' : null;
+
+  return (
+    <div data-testid="home-page">
+      <SearchBar
+        searchValue={searchValue}
+        onSearchBarChange={handleSearchBarChange}
+        onSearchBarSubmit={handleSearchBarSubmit}
+      />
+      {error && <div>Error occured</div>}
+      {isLoading ? <Spinner /> : <Cards cards={images} />}
+      {notFound}
+    </div>
+  );
+};
+
+// class HomePage extends React.Component<HomePageProps, HomePageState> {
+//   constructor(props: HomePageProps) {
+//     super(props);
+//     this.state = {
+//       searchValue: '',
+//       images: [],
+//       isLoading: true,
+//       error: null,
+//     };
+//     this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
+//     this.handleSearchBarSubmit = this.handleSearchBarSubmit.bind(this);
+//   }
+
+//   async fetchImages(searchValue: string) {
+//     this.setState({
+//       isLoading: true,
+//     });
+
+//     const params: SearchImagesParams = {
+//       tags: searchValue,
+//       extras: 'url_n,owner_name,date_taken,views',
+//       page: '1',
+//       sort: 'interestingness-desc',
+//       per_page: '100',
+//     };
+
+//     try {
+//       const fetchedImages = await flickr('photos.search', params);
+
+//       this.setState({
+//         images: fetchedImages.photos.photo.filter((item: Image) => item.url_n),
+//       });
+//       this.setState({
+//         error: null,
+//       });
+//     } catch (err) {
+//       this.setState({
+//         images: [],
+//       });
+//       this.setState({
+//         error: err as Error,
+//       });
+//     } finally {
+//       this.setState({
+//         isLoading: false,
+//       });
+//     }
+//   }
+
+//   handleSearchBarChange(value: string) {
+//     this.setState({ searchValue: value });
+//   }
+
+//   handleSearchBarSubmit() {
+//     this.fetchImages(this.state.searchValue);
+//   }
+
+//   async componentDidMount() {
+//     const localStorageValue = localStorage.getItem('searchValue');
+//     if (localStorageValue) {
+//       await this.setState({ searchValue: localStorageValue });
+//       await this.fetchImages(this.state.searchValue);
+//     } else {
+//       await this.fetchImages('nature,flowers');
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     localStorage.setItem('searchValue', this.state.searchValue);
+//   }
+
+//   render() {
+//     const images = this.state.images;
+//     const isLoading = this.state.isLoading;
+//     const error = this.state.error;
+//     const notFound = !error && !isLoading && images.length === 0 ? 'Nothing found' : null;
+
+//     return (
+//       <div data-testid="home-page">
+//         <SearchBar
+//           searchValue={this.state.searchValue}
+//           onSearchBarChange={this.handleSearchBarChange}
+//           onSearchBarSubmit={this.handleSearchBarSubmit}
+//         />
+//         {error && <div>Error occured</div>}
+//         {isLoading ? <Spinner /> : <Cards cards={this.state.images} />}
+//         {notFound}
+//       </div>
+//     );
+//   }
+// }
 
 export default HomePage;
