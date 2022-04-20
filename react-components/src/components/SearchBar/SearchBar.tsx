853439aadd6718ce60ccb9b13996c4388f2648c0
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-20 21:53:44 +0300

    feat: convert SearchBar to a function component

--- react-components/src/components/SearchBar/SearchBar.tsx
+++ react-components/src/components/SearchBar/SearchBar.tsx
@@ -8,42 +8,37 @@ type SearchBarProps = {
   onSearchBarSubmit: () => void;
 };
 
-type SearchBarState = Record<string, never>;
+export const SearchBar = ({
+  searchValue,
+  onSearchBarChange,
+  onSearchBarSubmit,
+}: SearchBarProps) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target;
+    onSearchBarChange(value);
+  };
 
-class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
-  constructor(props: SearchBarProps) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    this.props.onSearchBarChange(event.target.value);
-  }
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    if (this.props.searchValue) {
-      this.props.onSearchBarSubmit();
+    if (searchValue) {
+      onSearchBarSubmit();
     }
-  }
+  };
 
-  render() {
-    return (
-      <form className={style.searchbar} onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          className={style.searchbarInput}
-          value={this.props.searchValue}
-          onChange={this.handleChange}
-          placeholder="Search"
-        ></input>
-        <button type="submit" className={style.searchbarBtn} data-testid="search-btn">
-          Search
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={style.searchbar} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={style.searchbarInput}
+        value={searchValue}
+        onChange={handleChange}
+        placeholder="Search"
+      ></input>
+      <button type="submit" className={style.searchbarBtn} data-testid="search-btn">
+        Search
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
