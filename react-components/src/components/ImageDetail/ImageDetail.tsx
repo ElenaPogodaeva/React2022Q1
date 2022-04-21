03ed13dfcaae4db42816ba8ffcece01088671d83
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-22 00:07:59 +0300

    feat: convert ImageDetail to a function component

--- react-components/src/components/ImageDetail/ImageDetail.tsx
+++ react-components/src/components/ImageDetail/ImageDetail.tsx
@@ -2,43 +2,37 @@ import React from 'react';
 import { ImageInfo } from '../../types/types';
 import style from './ImageDetail.module.scss';
 
-type CardProps = {
+type ImageDetailProps = {
   imageInfo: ImageInfo;
   imageUrl: string;
   handleClose: () => void;
 };
 
-type CardState = Record<string, never>;
-
-class ImageDetail extends React.Component<CardProps, CardState> {
-  stopPropagation(e: React.MouseEvent<HTMLElement>) {
+export const ImageDetail = ({ imageInfo, imageUrl, handleClose }: ImageDetailProps) => {
+  const stopPropagation = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
-  }
+  };
+  const { title, owner, description, dates, views } = imageInfo;
 
-  render() {
-    const { title, owner, description, dates, views } = this.props.imageInfo;
-    const url = this.props.imageUrl;
-
-    return (
-      <div className={style.popup} data-testid="image-detail">
-        <div className={style.popupBody} onClick={this.props.handleClose}>
-          <div className={style.popupContent} onClick={this.stopPropagation}>
-            <img src={url} className={style.popupImg} />
-            <h1 className={style.popupTitle}>{title._content}</h1>
-            <p className={style.popupText}>By {owner.username}</p>
-            <p className={style.popupText}>Taken on {dates.taken}</p>
-            <p className={style.popupText}>Views: {views}</p>
-            <h2 className={style.popupSubtitle}>Description</h2>
-            <p
-              className={style.popupDesc}
-              dangerouslySetInnerHTML={{ __html: description._content }}
-            ></p>
-            <button className={style.closeBtn} onClick={this.props.handleClose}></button>
-          </div>
+  return (
+    <div className={style.popup} data-testid="image-detail">
+      <div className={style.popupBody} onClick={handleClose}>
+        <div className={style.popupContent} onClick={stopPropagation}>
+          <img src={imageUrl} className={style.popupImg} />
+          <h1 className={style.popupTitle}>{title._content}</h1>
+          <p className={style.popupText}>By {owner.username}</p>
+          <p className={style.popupText}>Taken on {dates.taken}</p>
+          <p className={style.popupText}>Views: {views}</p>
+          <h2 className={style.popupSubtitle}>Description</h2>
+          <p
+            className={style.popupDesc}
+            dangerouslySetInnerHTML={{ __html: description._content }}
+          ></p>
+          <button className={style.closeBtn} onClick={handleClose}></button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ImageDetail;
