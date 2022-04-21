a708b7d041b08ea923389d385d97ff435edb4f5a
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-22 00:05:41 +0300

    feat: convert Cards to a function component

--- react-components/src/components/Cards/Cards.tsx
+++ react-components/src/components/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import { Image, ImageInfo, ImageSize, SearchInfoParams } from '../../types/types';
 import style from './Cards.module.scss';
@@ -10,107 +10,200 @@ type CardsProps = {
   cards: Image[];
 };
 
-type CardsState = {
-  currentImageId: string;
-  imageInfo: ImageInfo | null;
-  imageUrl: string;
-  viewerIsOpen: boolean;
-  isLoading: boolean;
-};
+export const Cards = ({ cards }: CardsProps) => {
+  const [currentImageId, setCurrentImageId] = useState('');
+  const [imageInfo, setImageInfo] = useState<ImageInfo | null>(null);
+  const [imageUrl, setImageUrl] = useState('');
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-class Cards extends React.Component<CardsProps, CardsState> {
-  constructor(props: CardsProps) {
-    super(props);
-    this.state = {
-      currentImageId: '',
-      imageInfo: null,
-      imageUrl: '',
-      viewerIsOpen: false,
-      isLoading: true,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
-
-  async fetchImageInfo() {
-    const currentId = this.state.currentImageId;
-
+  const fetchImageInfo = async () => {
     const params: SearchInfoParams = {
-      photo_id: currentId,
+      photo_id: currentImageId,
     };
-
     try {
       const fetchedImageInfo = await flickr('photos.getInfo', params);
-      this.setState({
-        imageInfo: fetchedImageInfo.photo,
-      });
+      setImageInfo(fetchedImageInfo.photo);
     } catch (err) {
+      console.log('Error');
       console.error(err);
     }
-  }
+  };
 
-  async fetchImageSizes() {
-    const currentId = this.state.currentImageId;
-
+  const fetchImageSizes = async () => {
     const params: SearchInfoParams = {
-      photo_id: currentId,
+      photo_id: currentImageId,
     };
-
     try {
       const fetchedImageSizes = await flickr('photos.getSizes', params);
       const sizes = fetchedImageSizes.sizes.size;
 
-      this.setState({
-        imageUrl: sizes
-          .reverse()
-          .filter((s: ImageSize) => s.label === 'Small' || s.label === 'Large')[0].source,
-      });
+      setImageUrl(
+        sizes.reverse().filter((s: ImageSize) => s.label === 'Small' || s.label === 'Large')[0]
+          .source
+      );
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-  async handleClick(id: string) {
-    await this.setState({
-      currentImageId: id,
-      viewerIsOpen: true,
-      isLoading: true,
-    });
-    await this.fetchImageInfo();
-    await this.fetchImageSizes();
-    await this.setState({
-      isLoading: false,
-    });
-  }
+  // const fetchData = useCallback(async () => {
+  //   await fetchImageInfo();
+  //   await fetchImageSizes();
+  //   setIsLoading(false);
+  // }, [currentImageId]);
 
-  handleClose() {
-    this.setState({
-      viewerIsOpen: false,
-    });
-  }
+  // const fetchData = useCallback( async () => {
+  //   console.log('usecallback');
+  //
+  //   if (currentImageId) {
+  //     setIsLoading(true);
+  //    await fetchImageInfo();
+  //    await fetchImageSizes();
+  //   setIsLoading(false);
+  //   }
+  // }, [currentImageId]);
 
-  render() {
-    if (!this.props.cards.length) return null;
+  const fetchData = async () => {
+    if (currentImageId) {
+      setIsLoading(true);
+      await fetchImageInfo();
+      await fetchImageSizes();
+      setIsLoading(false);
+    }
+  };
 
-    return (
-      <>
-        <div className={style.cards} data-testid="cards">
-          {this.props.cards &&
-            this.props.cards.map((card) => (
-              <Card key={card.id} card={card} handleClick={this.handleClick} />
-            ))}
-        </div>
-        {this.state.viewerIsOpen && this.state.isLoading && <Spinner />}
-        {this.state.viewerIsOpen && !this.state.isLoading && (
-          <ImageDetail
-            imageInfo={this.state.imageInfo as ImageInfo}
-            imageUrl={this.state.imageUrl}
-            handleClose={this.handleClose}
-          />
-        )}
-      </>
-    );
-  }
-}
+  useEffect(() => {
+    fetchData();
+    console.log('useeffect');
+    console.log(currentImageId);
+    console.log(isLoading);
+  }, [currentImageId, viewerIsOpen]);
+
+  const handleClick = (id: string) => {
+    setCurrentImageId(id);
+
+    setViewerIsOpen(true);
+    setIsLoading(true);
+    console.log(currentImageId);
+  };
+
+  const handleClose = () => {
+    setViewerIsOpen(false);
+  };
+
+  if (!cards.length) return null;
+
+  return (
+    <>
+      <div className={style.cards} data-testid="cards">
+        {cards && cards.map((card) => <Card key={card.id} card={card} handleClick={handleClick} />)}
+      </div>
+      {viewerIsOpen && isLoading && <Spinner />}
+      {viewerIsOpen && !isLoading && (
+        <ImageDetail
+          imageInfo={imageInfo as ImageInfo}
+          imageUrl={imageUrl}
+          handleClose={handleClose}
+        />
+      )}
+    </>
+  );
+};
+
+// class Cards extends React.Component<CardsProps, CardsState> {
+//   constructor(props: CardsProps) {
+//     super(props);
+//     this.state = {
+//       currentImageId: '',
+//       imageInfo: null,
+//       imageUrl: '',
+//       viewerIsOpen: false,
+//       isLoading: true,
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//     this.handleClose = this.handleClose.bind(this);
+//   }
+
+//   async fetchImageInfo() {
+//     const currentId = this.state.currentImageId;
+
+//     const params: SearchInfoParams = {
+//       photo_id: currentId,
+//     };
+
+//     try {
+//       const fetchedImageInfo = await flickr('photos.getInfo', params);
+//       this.setState({
+//         imageInfo: fetchedImageInfo.photo,
+//       });
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+
+//   async fetchImageSizes() {
+//     const currentId = this.state.currentImageId;
+
+//     const params: SearchInfoParams = {
+//       photo_id: currentId,
+//     };
+
+//     try {
+//       const fetchedImageSizes = await flickr('photos.getSizes', params);
+//       const sizes = fetchedImageSizes.sizes.size;
+
+//       this.setState({
+//         imageUrl: sizes
+//           .reverse()
+//           .filter((s: ImageSize) => s.label === 'Small' || s.label === 'Large')[0].source,
+//       });
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+
+//   async handleClick(id: string) {
+//     await this.setState({
+//       currentImageId: id,
+//       viewerIsOpen: true,
+//       isLoading: true,
+//     });
+//     await this.fetchImageInfo();
+//     await this.fetchImageSizes();
+//     await this.setState({
+//       isLoading: false,
+//     });
+//   }
+
+//   handleClose() {
+//     this.setState({
+//       viewerIsOpen: false,
+//     });
+//   }
+
+//   render() {
+//     if (!this.props.cards.length) return null;
+
+//     return (
+//       <>
+//         <div className={style.cards} data-testid="cards">
+//           {this.props.cards &&
+//             this.props.cards.map((card) => (
+//               <Card key={card.id} card={card} handleClick={this.handleClick} />
+//             ))}
+//         </div>
+//         {this.state.viewerIsOpen && this.state.isLoading && <Spinner />}
+//         {this.state.viewerIsOpen && !this.state.isLoading && (
+//           <ImageDetail
+//             imageInfo={this.state.imageInfo as ImageInfo}
+//             imageUrl={this.state.imageUrl}
+//             handleClose={this.handleClose}
+//           />
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 export default Cards;
