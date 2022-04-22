dac60ea233933b1623e4b37b8dc8ced35d4dc2e7
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-04-22 15:04:16 +0300

    refactor: remove comments

--- react-components/src/components/Cards/Cards.tsx
+++ react-components/src/components/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import { Image, ImageInfo, ImageSize, SearchInfoParams } from '../../types/types';
 import style from './Cards.module.scss';
@@ -25,7 +25,6 @@ export const Cards = ({ cards }: CardsProps) => {
       const fetchedImageInfo = await flickr('photos.getInfo', params);
       setImageInfo(fetchedImageInfo.photo);
     } catch (err) {
-      console.log('Error');
       console.error(err);
     }
   };
@@ -47,23 +46,6 @@ export const Cards = ({ cards }: CardsProps) => {
     }
   };
 
-  // const fetchData = useCallback(async () => {
-  //   await fetchImageInfo();
-  //   await fetchImageSizes();
-  //   setIsLoading(false);
-  // }, [currentImageId]);
-
-  // const fetchData = useCallback( async () => {
-  //   console.log('usecallback');
-  //
-  //   if (currentImageId) {
-  //     setIsLoading(true);
-  //    await fetchImageInfo();
-  //    await fetchImageSizes();
-  //   setIsLoading(false);
-  //   }
-  // }, [currentImageId]);
-
   const fetchData = async () => {
     if (currentImageId) {
       setIsLoading(true);
@@ -75,17 +57,12 @@ export const Cards = ({ cards }: CardsProps) => {
 
   useEffect(() => {
     fetchData();
-    console.log('useeffect');
-    console.log(currentImageId);
-    console.log(isLoading);
   }, [currentImageId, viewerIsOpen]);
 
   const handleClick = (id: string) => {
     setCurrentImageId(id);
-
     setViewerIsOpen(true);
     setIsLoading(true);
-    console.log(currentImageId);
   };
 
   const handleClose = () => {
@@ -110,100 +87,5 @@ export const Cards = ({ cards }: CardsProps) => {
     </>
   );
 };
-
-// class Cards extends React.Component<CardsProps, CardsState> {
-//   constructor(props: CardsProps) {
-//     super(props);
-//     this.state = {
-//       currentImageId: '',
-//       imageInfo: null,
-//       imageUrl: '',
-//       viewerIsOpen: false,
-//       isLoading: true,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//     this.handleClose = this.handleClose.bind(this);
-//   }
-
-//   async fetchImageInfo() {
-//     const currentId = this.state.currentImageId;
-
-//     const params: SearchInfoParams = {
-//       photo_id: currentId,
-//     };
-
-//     try {
-//       const fetchedImageInfo = await flickr('photos.getInfo', params);
-//       this.setState({
-//         imageInfo: fetchedImageInfo.photo,
-//       });
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-
-//   async fetchImageSizes() {
-//     const currentId = this.state.currentImageId;
-
-//     const params: SearchInfoParams = {
-//       photo_id: currentId,
-//     };
-
-//     try {
-//       const fetchedImageSizes = await flickr('photos.getSizes', params);
-//       const sizes = fetchedImageSizes.sizes.size;
-
-//       this.setState({
-//         imageUrl: sizes
-//           .reverse()
-//           .filter((s: ImageSize) => s.label === 'Small' || s.label === 'Large')[0].source,
-//       });
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-
-//   async handleClick(id: string) {
-//     await this.setState({
-//       currentImageId: id,
-//       viewerIsOpen: true,
-//       isLoading: true,
-//     });
-//     await this.fetchImageInfo();
-//     await this.fetchImageSizes();
-//     await this.setState({
-//       isLoading: false,
-//     });
-//   }
-
-//   handleClose() {
-//     this.setState({
-//       viewerIsOpen: false,
-//     });
-//   }
-
-//   render() {
-//     if (!this.props.cards.length) return null;
-
-//     return (
-//       <>
-//         <div className={style.cards} data-testid="cards">
-//           {this.props.cards &&
-//             this.props.cards.map((card) => (
-//               <Card key={card.id} card={card} handleClick={this.handleClick} />
-//             ))}
-//         </div>
-//         {this.state.viewerIsOpen && this.state.isLoading && <Spinner />}
-//         {this.state.viewerIsOpen && !this.state.isLoading && (
-//           <ImageDetail
-//             imageInfo={this.state.imageInfo as ImageInfo}
-//             imageUrl={this.state.imageUrl}
-//             handleClose={this.handleClose}
-//           />
-//         )}
-//       </>
-//     );
-//   }
-// }
 
 export default Cards;
--- react-components/src/pages/HomePage/HomePage.tsx
+++ react-components/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from '../../components/Cards/Cards';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Spinner from '../../components/Spinner/Spinner';
@@ -10,9 +10,6 @@ export const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [images, setImages] = useState<Image[]>([]);
   const [error, setError] = useState<Error | null>(null);
-
-  const searchValueRef = useRef('');
-  searchValueRef.current = searchValue;
 
   const fetchImages = async (searchValue: string) => {
     setIsLoading(true);
@@ -44,16 +41,10 @@ export const HomePage = () => {
     } else {
       fetchImages('nature,flowers');
     }
-
-    // return () => {
-    //   console.log('unmount');
-    //   localStorage.setItem('searchValue', searchValueRef.current as string);
-    // };
   }, []);
 
   useEffect(() => {
     return () => {
-      console.log('unmount');
       localStorage.setItem('searchValue', searchValue);
     };
   }, [searchValue]);
@@ -81,97 +72,5 @@ export const HomePage = () => {
     </div>
   );
 };
-
-// class HomePage extends React.Component<HomePageProps, HomePageState> {
-//   constructor(props: HomePageProps) {
-//     super(props);
-//     this.state = {
-//       searchValue: '',
-//       images: [],
-//       isLoading: true,
-//       error: null,
-//     };
-//     this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
-//     this.handleSearchBarSubmit = this.handleSearchBarSubmit.bind(this);
-//   }
-
-//   async fetchImages(searchValue: string) {
-//     this.setState({
-//       isLoading: true,
-//     });
-
-//     const params: SearchImagesParams = {
-//       tags: searchValue,
-//       extras: 'url_n,owner_name,date_taken,views',
-//       page: '1',
-//       sort: 'interestingness-desc',
-//       per_page: '100',
-//     };
-
-//     try {
-//       const fetchedImages = await flickr('photos.search', params);
-
-//       this.setState({
-//         images: fetchedImages.photos.photo.filter((item: Image) => item.url_n),
-//       });
-//       this.setState({
-//         error: null,
-//       });
-//     } catch (err) {
-//       this.setState({
-//         images: [],
-//       });
-//       this.setState({
-//         error: err as Error,
-//       });
-//     } finally {
-//       this.setState({
-//         isLoading: false,
-//       });
-//     }
-//   }
-
-//   handleSearchBarChange(value: string) {
-//     this.setState({ searchValue: value });
-//   }
-
-//   handleSearchBarSubmit() {
-//     this.fetchImages(this.state.searchValue);
-//   }
-
-//   async componentDidMount() {
-//     const localStorageValue = localStorage.getItem('searchValue');
-//     if (localStorageValue) {
-//       await this.setState({ searchValue: localStorageValue });
-//       await this.fetchImages(this.state.searchValue);
-//     } else {
-//       await this.fetchImages('nature,flowers');
-//     }
-//   }
-
-//   componentWillUnmount() {
-//     localStorage.setItem('searchValue', this.state.searchValue);
-//   }
-
-//   render() {
-//     const images = this.state.images;
-//     const isLoading = this.state.isLoading;
-//     const error = this.state.error;
-//     const notFound = !error && !isLoading && images.length === 0 ? 'Nothing found' : null;
-
-//     return (
-//       <div data-testid="home-page">
-//         <SearchBar
-//           searchValue={this.state.searchValue}
-//           onSearchBarChange={this.handleSearchBarChange}
-//           onSearchBarSubmit={this.handleSearchBarSubmit}
-//         />
-//         {error && <div>Error occured</div>}
-//         {isLoading ? <Spinner /> : <Cards cards={this.state.images} />}
-//         {notFound}
-//       </div>
-//     );
-//   }
-// }
 
 export default HomePage;
