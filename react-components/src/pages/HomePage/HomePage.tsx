4b4997570c451fc66c019cf6330c91b4a1f48d5d
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-05-06 16:53:49 +0300

    feat: add useSelector to HomePage

--- react-components/src/pages/HomePage/HomePage.tsx
+++ react-components/src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,37 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Cards from '../../components/Cards/Cards';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Spinner from '../../components/Spinner/Spinner';
 import Pagination from '../../components/Pagination/Pagination';
-import { AppContext } from '../../context/context';
 import SearchOptions from '../../components/SearchOptions/SearchOptions';
+import { useAppSelector } from '../../hooks';
+
+type SearchOptions = {
+  searchValue: string;
+  sortBy: string;
+  perPage: number;
+  currentPage: number;
+};
 
 export const HomePage = () => {
-  const { state } = useContext(AppContext);
-  const { searchValue, images, error, isLoading } = state;
+  const { searchValue, images, error, isLoading } = useAppSelector((state) => state.search);
 
   const searchValueRef = useRef('');
   searchValueRef.current = searchValue;
+
+  // useEffect(() => {
+  //   const localStorageValue = localStorage.getItem('searchValue');
+  //   if (localStorageValue) {
+  //     //dispatch({ type: ActionType.SetSearchValue, payload: localStorageValue });
+  //     //fetchImages(localStorageValue, sortBy, resultsPerPage, currentPage);
+  //     dispatch(
+  //       fetchImages({ searchValue: localStorageValue, sortBy, resultsPerPage, currentPage })
+  //     );
+  //   } else {
+  //     //fetchImages('nature,flowers', sortBy, resultsPerPage, currentPage);
+  //     dispatch(fetchImages({ searchValue: 'nature,flowers', sortBy, resultsPerPage, currentPage }));
+  //   }
+  // }, [dispatch]);
 
   useEffect(() => {
     return () => {
